82368e753cee09541af6bc1d552c65bce88ae3db
dalaaAlomari01 <sdsdomari3@gmail.com>
2025-12-07 10:41:51 +0300

    Initialized class variables (socket and username)

--- project files/client.py
+++ project files/client.py
@@ -12,3 +12,13 @@ from tkinter import scrolledtext , messagebox
 SERVER_HOST = "127.0.0.1"      # Server settings (change HOST if the server is on another machine)
 SERVER_PORT = 5000             # Must match your server port
 
+class NewsClientGUI:
+    def __init__(self, root: tk.Tk):       # Initialize the GUI window and build the login screen.
+      self.root = root
+      self.root.title("News Client - GUI Version") 
+
+      self.sock: socket.socket | None = None       # TCP socket connection
+      self.username: str =""                        # User's chosen name
+      
+                       
+

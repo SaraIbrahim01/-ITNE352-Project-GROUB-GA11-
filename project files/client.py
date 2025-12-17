5845566be514316268e66f7e694c1abec839bd3e
dalaaAlomari01 <sdsdomari3@gmail.com>
2025-12-17 19:14:13 +0300

    creat NewsClientGUI class

--- project files/client.py
+++ project files/client.py
@@ -13,7 +13,7 @@ SERVER_HOST = "127.0.0.1"      # Server settings-> (change HOST if the server is
 SERVER_PORT = 5000             #  Must match your server port 
 
 class NewsClientGUI:
-    def __init__(self, root: tk.Tk):       # Initialize the GUI window and build the login screen.
+    def __init__(self, root: tk.Tk):       # Initialize the GUI window and build the  login screen.
       self.root = root
       self.root.title("News Client - GUI Version") 
 

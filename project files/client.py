0ff883a438610eb849b9ad58187cdf6161a2474d
dalaaAlomari01 <sdsdomari3@gmail.com>
2025-12-17 18:55:16 +0300

    add required python libraries (socket , threading, tkinter)

--- project files/client.py
+++ project files/client.py
@@ -10,7 +10,7 @@ from tkinter import scrolledtext , messagebox
 # Server settings (change HOST if the server is on another machine)
 
 SERVER_HOST = "127.0.0.1"      # Server settings (change HOST if the server is on another machine)
-SERVER_PORT = 5000             # Must match your server port
+SERVER_PORT = 5000             #  Must match your server port
 
 class NewsClientGUI:
     def __init__(self, root: tk.Tk):       # Initialize the GUI window and build the login screen.
